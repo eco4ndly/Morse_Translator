06e051dd71a184977fe444bfb10fbc18ab914485
Sayan Porya <sayan.porya@mantralabsglobal.com>
2020-04-30 10:51:01 +0530

    Codacy Refactor: to get rid of complex if-else ladder

--- app/src/main/java/com/eco4ndly/morse_translater/core/MorseMap.kt
+++ app/src/main/java/com/eco4ndly/morse_translater/core/MorseMap.kt
@@ -4,91 +4,140 @@ package com.eco4ndly.morse_translater.core
  * A Sayan Porya code on 23/04/20
  */
 object MorseMap {
+    private val morseToEnglishMap = HashMap<String, String>()
+    private val englishToMorseMap = HashMap<String, String>()
+
+
+    /**
+     * Converts english character to morse code
+     * This method turns the character to lowercase
+     *
+     * @param toEncode English character to encode
+     */
     fun englishToMorse(toEncode: String): String {
-        if (toEncode == " ") return " / "
-        //if (toEncode == "") return "|"
-        if (toEncode.equals("a", ignoreCase = true)) return ".-"
-        if (toEncode.equals("b", ignoreCase = true)) return "-..."
-        if (toEncode.equals("c", ignoreCase = true)) return "-.-."
-        if (toEncode.equals("d", ignoreCase = true)) return "-.."
-        if (toEncode.equals("e", ignoreCase = true)) return "."
-        if (toEncode.equals("f", ignoreCase = true)) return "..-."
-        if (toEncode.equals("g", ignoreCase = true)) return "--."
-        if (toEncode.equals("h", ignoreCase = true)) return "...."
-        if (toEncode.equals("i", ignoreCase = true)) return ".."
-        if (toEncode.equals("j", ignoreCase = true)) return ".---"
-        if (toEncode.equals("k", ignoreCase = true)) return "-.-"
-        if (toEncode.equals("l", ignoreCase = true)) return ".-.."
-        if (toEncode.equals("m", ignoreCase = true)) return "--"
-        if (toEncode.equals("n", ignoreCase = true)) return "-."
-        if (toEncode.equals("o", ignoreCase = true)) return "---"
-        if (toEncode.equals("p", ignoreCase = true)) return ".--."
-        if (toEncode.equals("q", ignoreCase = true)) return "--.-"
-        if (toEncode.equals("r", ignoreCase = true)) return ".-."
-        if (toEncode.equals("s", ignoreCase = true)) return "..."
-        if (toEncode.equals("t", ignoreCase = true)) return "-"
-        if (toEncode.equals("u", ignoreCase = true)) return "..-"
-        if (toEncode.equals("v", ignoreCase = true)) return "...-"
-        if (toEncode.equals("w", ignoreCase = true)) return ".--"
-        if (toEncode.equals("x", ignoreCase = true)) return "-..-"
-        if (toEncode.equals("y", ignoreCase = true)) return "-.--"
-        if (toEncode.equals("z", ignoreCase = true)) return "--.."
-        if (toEncode.equals("0", ignoreCase = true)) return "-----"
-        if (toEncode.equals("1", ignoreCase = true)) return ".----"
-        if (toEncode.equals("2", ignoreCase = true)) return "..---"
-        if (toEncode.equals("3", ignoreCase = true)) return "...--"
-        if (toEncode.equals("4", ignoreCase = true)) return "....-"
-        if (toEncode.equals("5", ignoreCase = true)) return "....."
-        if (toEncode.equals("6", ignoreCase = true)) return "-...."
-        if (toEncode.equals("7", ignoreCase = true)) return "--..."
-        if (toEncode.equals("8", ignoreCase = true)) return "---.."
-        if (toEncode.equals("9", ignoreCase = true)) return "----."
-        if (toEncode.equals(".", ignoreCase = true)) return ".-.-"
-        if (toEncode.equals(",", ignoreCase = true)) return "--..--"
-        if (toEncode.equals("?", ignoreCase = true)) return "..--.."
+        toEncode.toLowerCase().let {
+            if (englishToMorseMap.isEmpty()) {
+                englishToMorseMap.putAll(buildEnglishToMorseMap())
+            }
+            if (englishToMorseMap.containsKey(it)) {
+                return englishToMorseMap[it]!!
+            }
+
+            return ""
+        }
+    }
+
+    /**
+     * Converts morse code character to english
+     *
+     * @param toDecode Morse code character to decode
+     */
+    fun morseToEnglish(toDecode: String): String {
+        if (morseToEnglishMap.isEmpty()) {
+            morseToEnglishMap.putAll(buildMorseToEnglishMap())
+        }
+
+        if (morseToEnglishMap.containsKey(toDecode)) {
+            return morseToEnglishMap[toDecode]!!
+        }
+
         return ""
     }
 
-    fun morseToEnglish(toEncode: String): String? {
-        if (toEncode == ".-") return "a"
-        if (toEncode == "-...") return "b"
-        if (toEncode == "-.-.") return "c"
-        if (toEncode == "-..") return "d"
-        if (toEncode == ".") return "e"
-        if (toEncode == "..-.") return "f"
-        if (toEncode == "--.") return "g"
-        if (toEncode == "....") return "h"
-        if (toEncode == "..") return "i"
-        if (toEncode == ".---") return "j"
-        if (toEncode == "-.-") return "k"
-        if (toEncode == ".-..") return "l"
-        if (toEncode == "--") return "m"
-        if (toEncode == "-.") return "n"
-        if (toEncode == "---") return "o"
-        if (toEncode == ".--.") return "p"
-        if (toEncode == "--.-") return "q"
-        if (toEncode == ".-.") return "r"
-        if (toEncode == "...") return "s"
-        if (toEncode == "-") return "t"
-        if (toEncode == "..-") return "u"
-        if (toEncode == "...-") return "v"
-        if (toEncode == ".--") return "w"
-        if (toEncode == "-..-") return "x"
-        if (toEncode == "-.--") return "y"
-        if (toEncode == "--..") return "z"
-        if (toEncode == "-----") return "0"
-        if (toEncode == ".----") return "1"
-        if (toEncode == "..---") return "2"
-        if (toEncode == "...--") return "3"
-        if (toEncode == "....-") return "4"
-        if (toEncode == ".....") return "5"
-        if (toEncode == "-....") return "6"
-        if (toEncode == "--...") return "7"
-        if (toEncode == "---..") return "8"
-        if (toEncode == "----.") return "9"
-        if (toEncode == "|") return ""
-        if (toEncode == "/") return " "
-        if (toEncode == " ") return " "
-        return ""
+    private fun buildMorseToEnglishMap(): HashMap<String, String> {
+        val morseToEnglishMap = HashMap<String, String>()
+        morseToEnglishMap[".-"] = "a"
+        morseToEnglishMap["-..."] = "b"
+        morseToEnglishMap["-.-."] = "c"
+        morseToEnglishMap["-.."] = "d"
+        morseToEnglishMap["."] = "e"
+        morseToEnglishMap["..-."] = "f"
+        morseToEnglishMap["--."] = "g"
+        morseToEnglishMap["...."] = "h"
+        morseToEnglishMap[".."] = "i"
+        morseToEnglishMap[".---"] = "j"
+        morseToEnglishMap["-.-"] = "k"
+        morseToEnglishMap[".-.."] = "l"
+        morseToEnglishMap["--"] = "m"
+        morseToEnglishMap["-."] = "n"
+        morseToEnglishMap["---"] = "o"
+        morseToEnglishMap[".--."] = "p"
+        morseToEnglishMap["--.-"] = "q"
+        morseToEnglishMap[".-."] = "r"
+        morseToEnglishMap["..."] = "s"
+        morseToEnglishMap["-"] = "t"
+        morseToEnglishMap["..-"] = "u"
+        morseToEnglishMap["...-"] = "v"
+        morseToEnglishMap[".--"] = "w"
+        morseToEnglishMap["-..-"] = "x"
+        morseToEnglishMap["-.--"] = "y"
+        morseToEnglishMap["--.."] = "z"
+        morseToEnglishMap["-----"] = "0"
+        morseToEnglishMap[".----"] = "1"
+        morseToEnglishMap["..---"] = "2"
+        morseToEnglishMap["...--"] = "3"
+        morseToEnglishMap["....-"] = "4"
+        morseToEnglishMap["....."] = "5"
+        morseToEnglishMap["-...."] = "6"
+        morseToEnglishMap["--..."] = "7"
+        morseToEnglishMap["---.."] = "8"
+        morseToEnglishMap["----."] = "9"
+        morseToEnglishMap["|"] = ""
+        morseToEnglishMap["/"] = " "
+        morseToEnglishMap[" "] = " "
+        morseToEnglishMap[".-.-"] = "#"
+        morseToEnglishMap[".-.-.-"] = "."
+        morseToEnglishMap["--..--"] = ","
+        morseToEnglishMap["..--.."] = "?"
+
+        return morseToEnglishMap
     }
+
+    private fun buildEnglishToMorseMap(): HashMap<String, String> {
+        val englishToMorseMap = HashMap<String, String>()
+        englishToMorseMap["a"] = ".-"
+        englishToMorseMap["b"] = "-..."
+        englishToMorseMap["c"] = "-.-."
+        englishToMorseMap["d"] = "-.."
+        englishToMorseMap["e"] = "."
+        englishToMorseMap["f"] = "..-."
+        englishToMorseMap["g"] = "--."
+        englishToMorseMap["h"] = "...."
+        englishToMorseMap["i"] = ".."
+        englishToMorseMap["j"] = ".---"
+        englishToMorseMap["k"] = "-.-"
+        englishToMorseMap["l"] = ".-.."
+        englishToMorseMap["m"] = "--"
+        englishToMorseMap["n"] = "-."
+        englishToMorseMap["o"] = "---"
+        englishToMorseMap["p"] = ".--."
+        englishToMorseMap["q"] = "--.-"
+        englishToMorseMap["r"] = ".-."
+        englishToMorseMap["s"] = "..."
+        englishToMorseMap["t"] = "-"
+        englishToMorseMap["u"] = "..-"
+        englishToMorseMap["v"] = "...-"
+        englishToMorseMap["w"] = ".--"
+        englishToMorseMap["x"] = "-..-"
+        englishToMorseMap["y"] = "-.--"
+        englishToMorseMap["z"] = "--.."
+        englishToMorseMap["0"] = "-----"
+        englishToMorseMap["1"] = ".----"
+        englishToMorseMap["2"] = "..---"
+        englishToMorseMap["3"] = "...--"
+        englishToMorseMap["4"] = "....-"
+        englishToMorseMap["5"] = "....."
+        englishToMorseMap["6"] = "-...."
+        englishToMorseMap["7"] = "--..."
+        englishToMorseMap["8"] = "---.."
+        englishToMorseMap["9"] = "----."
+        englishToMorseMap["."] = ".-.-.-"
+        englishToMorseMap["#"] = ".-.-"
+        englishToMorseMap[","] = "--..--"
+        englishToMorseMap["?"] = "..--.."
+        englishToMorseMap[" "] = " / "
+
+        return englishToMorseMap
+    }
+
 }
