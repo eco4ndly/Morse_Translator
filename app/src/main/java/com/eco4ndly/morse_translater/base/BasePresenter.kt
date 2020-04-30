a9053392f35b12dce2785c34b2980f427183237b
Sayan Porya <sayan.porya@mantralabsglobal.com>
2020-04-30 10:34:22 +0530

    Codacy Enhancement: Added comment to public method

--- app/src/main/java/com/eco4ndly/morse_translater/base/BasePresenter.kt
+++ app/src/main/java/com/eco4ndly/morse_translater/base/BasePresenter.kt
@@ -4,7 +4,16 @@ package com.eco4ndly.morse_translater.base
  * A Sayan Porya code on 28/04/20
  */
 interface BasePresenter<T: BaseView> {
+
+    /**
+     * When view is ready, add it to the presenter using this method
+     *
+     */
     fun subscribeView(view: T)
 
+    /**
+     * When view instance is not available anymore, destroyed most probably. Call this method to
+     * remove the previously injected view to the presenter
+     */
     fun unSubscribeView()
 }
